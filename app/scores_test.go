6497ac03e5f598db6be13d73c677bf5fd04993ec
moulid15 <moulid15@gmail.com>
2024-10-15 17:55:02 -0700

    add new tests

--- app/scores_test.go
+++ app/scores_test.go
@@ -2,16 +2,21 @@ package app
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestScores(t *testing.T) {
 	game := Game{}
 	yes, err := game.GetScores("NFL", "2024-10-14")
 
-	t.Log("payload", yes)
+	t.Log("payload", yes[0])
 
-	if err == nil {
+	if err != nil {
 		t.Error()
 	}
+
+	assert.Equal(t, yes[0].Team, "Bills")
+	assert.Equal(t, yes[0].Op, "Jets")
 
 }
